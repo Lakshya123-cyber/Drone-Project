2ce110c44e9c700681f1f19697b3fe4daf3e1044
Lakshya Raikwal <72344300+Lakshya123-cyber@users.noreply.github.com>
2021-10-30 11:30:17 +0800

    Add files via upload

--- movement/movement.py
+++ movement/movement.py
@@ -1,1 +1,27 @@
+# WORKS SUCCESSFULLY
 
+# 1. Have a drone
+# 2. App setup and Test Run
+    #  App -> TELLO
+    # settings -> More -> Firmware update
+
+# 3. Basic Movement
+    # install djitello
+    # install opencv
+from djitellopy import tello
+from time import sleep
+
+me = tello.Tello()
+me.connect()
+# print(me.get_battery())
+
+me.takeoff()
+
+# me.move_forward(30)
+me.send_rc_control(0, 50, 0, 0) # left/right, front/back, up/down, rotation
+sleep(2)
+me.send_rc_control(30, 0, 0, 0)
+sleep(2)
+me.send_rc_control(0, 0, 0, 0)
+sleep(2)
+me.land()
