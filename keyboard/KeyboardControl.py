b9b15544e2920247b739b719c3a9def8b665e4b8
Lakshya Raikwal <72344300+Lakshya123-cyber@users.noreply.github.com>
2021-10-30 11:29:49 +0800

    Add files via upload

--- keyboard/KeyboardControl.py
+++ keyboard/KeyboardControl.py
@@ -1,1 +1,48 @@
+# WORKS SUCCESSFULLY
 
+from djitellopy import tello
+import KeyPressModule as kp
+from time import sleep
+
+kp.init()
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+
+def getKeyboardInput():
+    lr, fb, ud, yv = 0, 0, 0, 0
+    speed = 50
+
+    if kp.getKey("LEFT"):
+        lr = -speed
+    elif kp.getKey("RIGHT"):
+        lr = speed
+
+    if kp.getKey("UP"):
+        fb = speed
+    elif kp.getKey("DOWN"):
+        fb = -speed
+
+    if kp.getKey("w"):
+        ud = speed
+    elif kp.getKey("s"):
+        ud = -speed
+
+    if kp.getKey("a"):
+        yv = speed
+    elif kp.getKey("d"):
+        yv = -speed
+
+    if kp.getKey("l"):
+        me.land()
+    if kp.getKey("t"):
+        me.takeoff()
+
+    return [lr, fb, ud, yv]
+
+me.takeoff()
+
+while True:
+    vals = getKeyboardInput()
+    me.send_rc_control(vals[0], vals[1], vals[2], vals[3]) # left/right, front/back, up/down, rotation
+    sleep(0.05)
